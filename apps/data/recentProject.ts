c6b17e696dacf17bf1ad8ee5d92c938d691804e3
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-09-24 17:16:21 +0700

    feature: add example data

--- apps/data/recentProject.ts
+++ apps/data/recentProject.ts
@@ -3,5 +3,15 @@ import { IRecentProject } from "@interfaces/index";
 export const RecentProject: IRecentProject[] = [
     { id: 1, fileImage: "smeshub", title: "Smeshub Indonesia", description: "Platform support SME. All will sale their product to customer and communication" },
     { id: 2, fileImage: "onepane", title: "Onepane AI", description: "Onepane Company Profile focusing in provide service about Loggin and Monitoring Tools" },
-    { id: 1, fileImage: "timvoice", title: "Timvoice", description: "Platform Invoicing help a freelancer with business owner with paperless document" },
+    { id: 3, fileImage: "timvoice", title: "Timvoice", description: "Platform Invoicing help a freelancer with business owner with paperless document" },
+]
+
+export const AllProject: IRecentProject[] = [
+    { id: 1, fileImage: "smeshub", title: "Smeshub Indonesia", description: "Platform support SME. All will sale their product to customer and communication" },
+    { id: 2, fileImage: "halisoft", title: "Halisoft Recruitment", description: "Platform to find candidate or talent who fit with company requirement" },
+    { id: 3, fileImage: "onepane", title: "Onepane AI", description: "Onepane Company Profile focusing about Logging and Monitoring Tools" },
+    { id: 4, fileImage: "pokemon", title: "Pokemon App", description: "Pokemon landing page and detail information about pokemon" },
+    { id: 5, fileImage: "cyberpunk", title: "Cyberpunk Store", description: "Small part of marketplace feature like add product to cart and remove quantity" },
+    { id: 6, fileImage: "salespack", title: "Salespack", description: "Landing page about packaging product explain benefit and other" },
+    { id: 7, fileImage: "timvoice", title: "Timvoice", description: "Platform Invoicing help a freelancer with business owner with paperless document" },
 ]
