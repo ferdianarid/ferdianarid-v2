e3273aa62792c0bcb4193ed1c21db79e30ce0dc7
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-09-28 00:37:03 +0700

    feature: custom error page and add dark mode in auth nav

--- pages/auth/signup/SignUp.tsx
+++ pages/auth/signup/SignUp.tsx
@@ -69,39 +69,39 @@ const SignUp: NextPage = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <AuthLayout>
-                <div className="w-full bg-primary-pressed text-neutral-10 px-6 md:px-24 flex pb-10 flex-col md:flex-row justify-between md:justify-between md:space-x-16 items-center">
-                    <div className="w-full md:w-1/2 mt-3 mx-auto px-10 py-10 rounded-3xl border border-primary-hover">
-                        <h1 className="text-2xl font-gilroy-semibold text-neutral-10 text-center">Create your Account</h1>
-                        <h1 className="text-sm mt-2 font-gilroy-normal text-neutral-10 text-center">Lets create account first to access all features</h1>
+                <div className="w-full bg-neutral-10 dark:bg-primary-pressed text-primary-pressed dark:text-neutral-10 px-6 md:px-24 flex pb-10 flex-col md:flex-row justify-between md:justify-between md:space-x-16 items-center">
+                    <div className="w-full md:w-1/2 mt-3 mx-auto px-10 py-10 rounded-3xl border border-neutral-30 dark:border-primary-hover">
+                        <h1 className="text-2xl font-gilroy-semibold text-primary-pressed dark:text-neutral-10 text-center">Create your Account</h1>
+                        <h1 className="text-sm mt-2 font-gilroy-normal text-primary-pressed dark:text-neutral-10 text-center">Lets create account first to access all features</h1>
                         <Formik initialValues={initialValues} validationSchema={schemaValidations} onSubmit={SignUp}>
                             <Form>
                                 <div className="mt-4">
-                                    <p className="text-sm font-gilroy-medium text-neutral-10">Email Address <span className="text-semantic-danger-main">*</span></p>
-                                    <Field className="text-md bg-primary-hover focus:bg-primary-pressed border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="email" name="email" id="email" placeholder="Email Address" />
+                                    <p className="text-sm font-gilroy-medium text-primary-pressed dark:text-neutral-10">Email Address <span className="text-semantic-danger-main">*</span></p>
+                                    <Field className="text-md text-primary-pressed dark:text-neutral-10 bg-slate-100 dark:bg-primary-hover dark:focus:bg-primary-pressed focus:bg-neutral-10 border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="email" name="email" id="email" placeholder="Email Address" />
                                     <ErrorMessage name="email" component={ErrorText} />
                                 </div>
 
                                 <div className="mt-4">
-                                    <p className="text-sm font-gilroy-medium text-neutral-10">Password <span className="text-semantic-danger-main">*</span></p>
-                                    <Field className="text-md bg-primary-hover focus:bg-primary-pressed border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="password" name="password" id="password" placeholder="Password" />
+                                    <p className="text-sm font-gilroy-medium text-primary-pressed dark:text-neutral-10">Password <span className="text-semantic-danger-main">*</span></p>
+                                    <Field className="text-md text-primary-pressed dark:text-neutral-10 bg-slate-100 dark:bg-primary-hover dark:focus:bg-primary-pressed focus:bg-neutral-10 border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="password" name="password" id="password" placeholder="Password" />
                                     <ErrorMessage name="password" component={ErrorText} />
                                 </div>
 
                                 <div className="mt-4">
-                                    <p className="text-sm font-gilroy-medium text-neutral-10">Confirmation Password <span className="text-semantic-danger-main">*</span></p>
-                                    <Field className="text-md bg-primary-hover focus:bg-primary-pressed border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmation Password" />
+                                    <p className="text-sm font-gilroy-medium text-primary-pressed dark:text-neutral-10">Confirmation Password <span className="text-semantic-danger-main">*</span></p>
+                                    <Field className="text-md text-primary-pressed dark:text-neutral-10 bg-slate-100 dark:bg-primary-hover dark:focus:bg-primary-pressed focus:bg-neutral-10 border-none mt-2 py-3 px-4 rounded-lg w-full focus:outline-none focus:border-2 focus:outline-semantic-warning-main" type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmation Password" />
                                     <ErrorMessage name="confirmPassword" component={ErrorText} />
                                 </div>
 
                                 <div className="mt-8">
-                                    <button type="submit" className="py-3 font-gilroy-bold px-4 rounded-lg w-full bg-semantic-warning-main">
+                                    <button type="submit" className="py-3 text-neutral-10 font-gilroy-bold px-4 rounded-lg w-full bg-semantic-warning-main">
                                         {isRegisterLoading ? "Creating Account ..." : "Sign Up"}
                                     </button>
                                 </div>
                             </Form>
                         </Formik>
 
-                        <p className="text-sm mt-4 font-gilroy-normal text-neutral-10 text-center">Or sign up using</p>
+                        <p className="text-sm mt-4 font-gilroy-normal text-primary-pressed dark:text-neutral-10 text-center">Or sign up using</p>
 
                         <div className="w-full flex flex-col md:flex-row items-center justify-between md:space-x-2 space-y-3 md:space-y-0 mt-4">
                             <button onClick={SignInWithGoogle} className="py-3 font-gilroy-bold px-4 rounded-lg w-full text-sm flex items-center justify-center gap-x-3 border border-primary-hover">
@@ -113,7 +113,7 @@ const SignUp: NextPage = () => {
                                 {isGithubLoading ? "Authenticate ..." : "Sign Up with Github"}</button>
                         </div>
 
-                        <p className="text-sm mt-4 font-gilroy-normal text-neutral-10 text-center">Already have account ?
+                        <p className="text-sm mt-4 font-gilroy-normal text-primary-pressed dark:text-neutral-10 text-center">Already have account ?
                             <Link href="/auth/signin">
                                 <span className="font-gilroy-bold ml-2 text-blue-500 hover:cursor-pointer">Sign In Account</span>
                             </Link>
