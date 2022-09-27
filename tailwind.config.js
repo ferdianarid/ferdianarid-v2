483a0c34f3bb9d744ecdaa2ca29b317160eb6de1
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-09-27 22:36:01 +0700

    feature: add libs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
+    darkMode: "class",
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./apps/components/**/*.{js,ts,jsx,tsx}",
